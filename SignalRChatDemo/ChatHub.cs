ce69a56773443f178c6f60602f346b74e5ce334f
levani vanishvili <levan.vanishvili@gmail.com>
2020-10-16 10:37:23 +0400

    Add class ChatHub

---
+++ SignalRChatDemo/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChatDemo
+{
+    public class ChatHub: Hub
+    {
+        // receives some sent message as a parameter and then with await call Clients.All.SendAsync ("Send", message, userName) relay this message to all connected clients.
+        public async Task Send(string message, string userName)
+        {
+            await this.Clients.All.SendAsync("Send", message, userName);
+        }
+    }
+}
